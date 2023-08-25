2b8c6bd49d2d1bbeff5368f44f7cb2d8b6351957
Shreyascr5 <shreyas050801@gmail.com>
2023-08-25 14:03:28 +0530

    created car component

--- src/Components/Cars.js
+++ src/Components/Cars.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import car from "../Images/paganihayura.webp";
+import data from ".././Data.js";
 
 function Cars() {
+  const car=data.map((e)=>{
+    const{id,title,img}=e;
+    return(
+      <div className='car-container'>
+      <img src={img} alt="car" width={300} height={150} />
+      <h1>{title}</h1>
+      <p>Model-2023</p>
+      <br />
+      <button>View more</button>
+    </div>
+    )
+  })
   return (
-    <div>Cars</div>
+  <>
+   <h1 className='car-header'>The greatest supercars</h1>
+  
+   <div className='car-grid'> {car}</div>
+   </>
+   
+    
   )
 }
 
---
+++ src/Data.js
@@ -0,0 +1,130 @@
+export default [
+    {
+      id: 1,
+      title: 'McLaren F1 ',
+      img: 'https://robbreport.com/wp-content/uploads/2019/12/6092563g.jpg?w=1000',
+      info:'Ok, so the first one on this list is technically from the last century, the 1990s to be exact, but it’s here as a benchmark and baseline for the models that follow. A top speed of 231 mph. Back in 1992, no other production car had ever gone that fast. It was mind-blowing. But that’s what the McLaren F1 did; blow minds. With its feather-weight carbon-fiber chassis, single-minded focus on shaving weight and a bespoke six-liter, 627 hp BMW V-12 for power, it could sear to 60 mph in just 3.2 seconds.',
+    },
+    {
+      id: 2,
+      title: 'Bugatti Veyron Super Sport ',
+      img: 'https://robbreport.com/wp-content/uploads/2019/12/4054408a.jpg?w=1000',
+      info:'Bugatti stormed back to the modern age from the pages of automotive history with the 2005 Veyron, which announced its intentions with quad-turbo twin-V8s that made an astonishing 1,200 hp. Fast forward a half-decade, and the Super Sport version of the Veyron promptly claimed the Guinness Book of Records title in 2010 as world’s fastest production car with a 260 mph run at Volkswagen’s Wolfsburg test track.'
+    },
+    {
+      id: 3,
+      title: 'Ferrari LaFerrari ',
+      img: 'https://robbreport.com/wp-content/uploads/2019/12/9863701f.jpg?w=1000',
+      info:'2013 was an auspicious year for supercars, with no fewer than three major releases debuting from McLaren, Porsche, and Ferrari and earning the “Holy Trinity” nickname. Though fiercely individual, each of the trio claimed a hybrid power-train layout.Of the three, only the Ferrari LaFerrari boasted a V-12 engine— and a raucous, naturally aspirated one, at that. The LaFerrari also happened to be the most powerful (and, unofficially at least) the most charismatic of the wild bunch. Eponymously named to suggest it was the quintessence of the Ferrari nameplate, the 950 hp hypercar may go down in history not only as the pinnacle of its era, but also as one of the greatest prancing horses of all time.'
+    },
+    {
+      id: 4,
+      title: 'McLaren P1',
+      img: 'https://robbreport.com/wp-content/uploads/2019/12/9370122a.jpg?w=1000',
+      info:'Of the three renowned hybrid hypercars that debuted in 2013, two (the Ferrari LaFerrari and Porsche 918 Spyder) hailed from long-established carmakers, while the other—the McLaren P1—was a relative newbie on the scene. Not that the British manufacturer hadn’t earned itself a spot in the hypercar pantheon with the 1990s-era legendary F1, but the lengthy absence made building this flagship like starting from scratch.McLaren used advanced carbon-fiber construction based on its lesser, more approachable (relatively) offerings, but the top dog P1 claimed a screaming 903 hp and a remarkably lightweight chassis, which made it a more than worthy contender against the supercar establishment of the time.'
+    },
+    {
+      id: 5,
+      title: 'Porsche 918 Spyder ',
+      img: 'https://robbreport.com/wp-content/uploads/2019/12/1561526743.jpg?w=1000',
+      info:'The 918 Spyder was a true game changer, demonstrating the potential of plug-in hybrid technology in the supercar stratosphere. A naturally aspirated, 4.6-liter V-8 with 599 hp got added power from two electric motors, for a total output of 877 hp and 944 ft lbs of near instant-on torque. Penned by Porsche’s chief designer, Michael Mauer, the 918 was first shown at the Geneva Motor Show in 2010 as a concept to gauge market interest, going into production in late 2013 with a base MSRP of $845,000. The entire allocation of—surprise—918 units, sold out by the end of 2014, so eager were VIP Porschephiles to acquire the most powerful street-going Porsche ever made. Production ended by mid-2015, and the 918 remains a highly desirable collector car today.'
+    },
+    {
+      id:6,
+      title:'Koenigsegg Regera',
+      img:'https://robbreport.com/wp-content/uploads/2019/12/1158128620.jpg?w=1000',
+      info:'Sweden may be known largely as the home of proletarian transportation king Volvo, but sports car enthusiasts also know it as the land of Koenigsegg. And one of the mightiest models to leave that factory is the Regera, brainchild of company founder Christian von Koenigsegg who was, reportedly, inspired after a few sprints in a Tesla Model S P95.The Regera, however, runs on hybrid power combining three electric motors with a 5-liter V-8. The power-train configuration, in total, produces a staggering 1,700 hp—all of which completely and wonderfully upends the image of Swedes being models of self-restraint',
+    },
+    {
+      id:7,
+      title:'Lamborghini Huracán Performante',
+      img:'https://robbreport.com/wp-content/uploads/2019/12/299790507.jpg?w=1000',
+      info:'Lamborghini’s Huracán was no slouch, a flying wedge of a car powered by a 10-cylinder engine producing a touch over 600 hp. But four years after the model’s debut in 2014, it was decided that this Italian raging bull needed a track-focused makeover. Carbon-fiber bumpers, skirts and an adjustable wing removed weight and added downforce, while an engine massage bumped horsepower up 5 percent and took the coupe’s zero-to-60 mph time down to just less than 3 seconds.The interior was just as race oriented, with sports seats and digital speedometer off the Aventador. All that added up to a brief but celebrated 2016 track record at Germany’s haloed Nürburgring, not bad at all for a company whose roots were in tractors. '
+    },
+    {
+      id:8,
+      title:'Pagani Huayra Roadster',
+      img:'https://robbreport.com/wp-content/uploads/2019/12/618447122.jpg?w=1000',
+      info:'Follow-ups to massive debuts are tough nuts to crack. But Argentine engineer Horatio Pagani did just that with the Huayra, the successor to his stop-the-presses Zonda. Back in the engine bay is a monstrous AMG-tuned power plant, this time a six-liter twin-turbo V-12 producing 738 hp.Wrapped around the driver and passenger of this roofless wonder is a tub made from a combination of carbon fiber and titanium for greater stiffness and lighter weight. But those are just the raw specs for a vehicle perhaps best known for being as meticulously refined as a Swiss watch, a true tastemaker’s triumph. '
+    },
+    {
+      id:9,
+      title:'Ruf CTR 30th Anniversary Edition',
+      img:'https://robbreport.com/wp-content/uploads/2019/12/8472954g.jpg?w=1000',
+      info:'Ruf’s 211 mph Yellowbird may have looked like a slightly tweaked Porsche 911, but the heavily modified creation was nothing short of groundbreaking, vanquishing titans like the Ferrari F40 and Lamborghini Countach in a 1987 Road & Track magazine cover story. Its legend became cemented with a Nürburgring-shredding session in the “Faszination” video.The latest CTR, marking its 30th anniversary, once again appears approachable at first glance, but this time around it boasts an entirely bespoke carbon-fiber chassis hiding a cornucopia of exotic hardware, from inboard suspension to a quick-revving 700 hp power plant. At once classic and futuristic, the CTR marks a momentous milestone against the original Yellowbird, offering a wildly capable supercar that hides in a discreetly familiar body.'
+    },
+    {
+      id:10,
+      title:'McLaren Senna',
+      img:'https://robbreport.com/wp-content/uploads/2021/12/mclaren-side.jpeg?w=1000',
+      info:'Being named after the legendary Formula 1 world champion Ayrton Senna guarantees the fearsome McLaren Senna rarified status. But it’s the car’s savage, rock-out-of-a-catapult performance and breathtaking capability, both on and off the racetrack, that heighten its near-mythical appeal.Unveiled in 2017 with production set at 500 examples, the $960,000 Senna is a road-legal, super-lightweight track missile powered by a 789 hp, mid-mounted four-liter twin-turbo V-8. It can run zero-to-62 mph in just 2.8 seconds and hit a top speed of 211 mph. While we think the F1 still ranks as the ultimate McLaren, the Senna is certainly right behind it. '
+    },
+    {
+      id:11,
+      title:'Ferrari SF90 Stradale',
+      img:'https://robbreport.com/wp-content/uploads/2019/12/sf90-stradale.jpg?w=1000',
+      info:'While the days of Maranello’s 12-cylinder halo rockets may be fading in today’s eco-climate, the eight-cylinder SF90 Stradale more than delivers. Billed as a street car tribute to Ferrari’s SF90 Formula 1 machine, the SF90 Stradale is an unabashed hypercar boasting 1,000 hp from three electric motors and a twin-turbo V-8.Its combination of exceptional hybrid power-train performance and dramatic looks pull from the best of existing aft-engined models. Note the nod to the 488’s flank scoops as well as to the marque’s racing pedigree—the nose simply screams motorsport, which this car salutes by name: Scuderia Ferrari, 90 years.'
+    },
+    {
+      id:12,
+      title:'Bugatti Chiron Super Sport 300+',
+      img:'https://robbreport.com/wp-content/uploads/2021/12/1-23.jpg?w=1000',
+      info:'On August 2, 2019, Bugatti’s Chiron Super Sport became the first series-production automobile to break the 300 mph mark (more precisely, 304.773 mph) on a German test track with official Bugatti test driver and Le Mans–winner Andy Wallace behind the wheel. Inspired by that achievement, the manufacturer created the eponymously named Chiron Super Sport 300+.Limited to 30 examples worldwide, it’s distinguished by carbon bodywork that extends the longtail by 9.8 inches, along with aero enhancements that improve high-speed performance by further lowering drag and increasing downforce. It’s about power too, where the 8.0-liter, W-16 quad-turbocharged engine produces 1,600 hp, 100 hp more than a “garden-variety” Chiron. As regards fuel consumption, depending on how fast you drive, your mileage may vary. '
+    },
+    {
+      id:13,
+      title:'SSC Tuatara',
+      img:'https://robbreport.com/wp-content/uploads/2019/12/ssc-tuatara.jpg?w=1000',
+      info:'To reach 300 mph. That’s the target that Washington State–based SSC North America has for its new SSC Tuatara hypercar. To hit that mark, the carbon-fiber–bodied Tuatara—named after a spiny lizard found in New Zealand—carries a 5.9-liter twin-turbo V-8 packing a massive 1,726 hp.Production has already kicked off with the goal to build 100 examples, each priced at $1.6 million. SSC isn’t new to the high-speed business. In 2007, its 1,287 hp Ultimate Aero clocked 256.14 mph. The record stood for three years before Bugatti’s Veyron Super Sports came along. But on January 17, 2021, the SSC Tuatara took the record back with two runs that averaged 282.9 mph, and the results were verified by Racelogic. More recently still, it officilly clocked 295 mph.'
+    },
+    {
+      id:14,
+      title:'Lamborghini Essenza SCV12',
+      img:'https://robbreport.com/wp-content/uploads/2021/12/2-22.jpg?w=1000',
+      info:'While Lamborghini’s lineup of 21st century road cars is more outlandish than ever, nothing earns street cred more than how track-worthy a model is. Enter the Essenza SCV12, Lambo’s postmodern hat tip to track specials of yore like the Diablo GTR and Miura Jota.Freed from the constraints of road legality, the Essenza uncorks Lamborghini’s naughtiest impulses from bow to stern. Extreme bodywork yields over 2,600 pounds of downforce, more than a GT3 race car and, liberated from emissions equipment, the hulking 6.5-liter, 819 hp engine is Lambo’s most powerful V-12 yet. Then there’s the futurist cockpit, which features a button-and-dial-clad steering yoke that wouldn’t be out of place in a Formula 1 car. And although the 40 buyers of the $2.6 million Essenza won’t have their own competition series, Lamborghini promises Essenza-only track days that should satisfy the owners’ race cravings.',
+    },
+    {
+      id:15,
+      title:'Aston Martin Valkyrie',
+      img:'https://robbreport.com/wp-content/uploads/2019/12/aston_martin_valkyrie_1-jpg.jpg?w=1000',
+      info:'Supercar greatness in the form of Aston Martin’s Valkyrie is now in production The model is a new benchmark for the automaker when it comes to street-legal, production-car performance. It’s what happens when you bolt a 1,000 hp, 6.5-liter V-12, along with a 160 hp Rimac-developed hybrid-electric system, into a lightweight, super-strong carbon monocoque.And if that wasn’t impressive enough, remember the car has been designed by Adrian Newey, a rock star in Formula 1 design and the current chief technical officer for Red Bull Racing. Production will be limited to 150 examples, each costing $3.2 million.',
+    },
+    {
+      id:16,
+      title:'Lamborghini Countach LPI 800-4',
+      img:'https://robbreport.com/wp-content/uploads/2021/12/2-10.jpeg?w=1000',
+      info:'It’s been a half century since Lamborghini gobsmacked the supercar world with the Countach and its wedge-shaped silhouette. Now, more than 50 years later, the Countach LPI 800-4 revives the iconic nameplate with a similar-but-not-the-same take on the model’s age-old formula.Some haters have dismissed the new Countach as a needless nod to the past; not only is the name dear to enthusiasts’ hearts, but so are signature design cues like the massive side-mounted ducts and air inlets at the haunches. However, the heart of the LPI 800-4 speaks more to Lamborghini’s hereafter than any sheetmetal could, namely an 802 hp, hybrid power train that seals the deal on the future relevance of the Raging Bull marque.',
+    },
+    {
+      id:17,
+      title:'Rimac Nevera',
+      img:'https://robbreport.com/wp-content/uploads/2021/12/nevera01.jpeg?w=1000',
+      info:'Landmark cars often hail from unexpected places, but the Rimac Nevera has dealt earth-shattering blows to the supercar microcosm. For starters, the battery-powered Nevera obliterated internal combustion records by routing 1,914 hp to all four wheels, eclipsing zero-to-60 mph times of everything from McLarens to Koenigseggs. More shockingly, the EV hypercar is the brainchild of 33-year old Croatian wunderkind Mate Rimac, who founded the firm in 2011.The Rimac Nevera’s early impact arose from its sensational performance stats, but the legacy of this hypercar will transcend a mere model. In summer 2021, the Croatian startup purchased a majority stake in Bugatti, marking the first (and likely not the last) time a legacy supercar brand falls under the control of an EV upstart. ',
+    },
+    {
+      id:18,
+      title:'Mercedes-AMG One',
+      img:'https://robbreport.com/wp-content/uploads/2021/12/amgone01.jpeg?w=1000',
+      info:'How can a car that just entered production rank as one of the supercar “greats” of the 21st century? Because we’re oh-so-confident that the 1,000 hp Mercedes-AMG Formula 1 racer for the street appears next summer, it continue to be mind-blowing for years to come.Unveiled back in 2017 as the Project One concept, this road-going monster was plagued by technical challenges, but then there’s a lot going on when you basically build a Formula 1 car you can take on the freeway.Powered by a hybrid-boosted, 1.6-liter turbo V-6 and a trio of electric motors, it’s expected to cover zero to 124 mph in less than 6 seconds and top out at 217 mph. Not surprisingly, all 275 examples of these $2.6 million tour de forces are taken. ',
+    },
+    {
+      id:19,
+      title:'Koenigsegg Jesko',
+      img:'https://robbreport.com/wp-content/uploads/2021/12/jesko-b.jpeg?w=1000',
+      info:'Back in 2017, Sweden’s Christian von Koenigsegg saw his Agera RS become the world’s fastest production car with a two-way top speed of 277.9 mph. The Agera’s successor, the mega-winged, 1,660 hp Jesko—named after Christian’s father—may have what it takes to top the Bugatti Chiron Super Sport’s 304.7 mph mark.The $3 million Jesko’s go-fast technology includes a screaming, 5.0-liter twin-turbo V-8, which features the world’s lightest V-8 crankshaft, weighing a mere 28 pounds. It’s no wonder that all 125 examples of the model scheduled for production have been presold.',
+    },
+    {
+      id:20,
+      title:'Pininfarina Battista',
+      img:'https://robbreport.com/wp-content/uploads/2021/12/29.jpg?w=1000',
+      info:'Automotive nameplates don’t come more legendary than Pininfarina. The Italian studio’s 62-year association with Ferrari, for example, created such icons as the 275 GTB, 365 GTB/4 Daytona and that Tom Selleck Magnum P.I. classic, the 308 GTS. The Cadillac Atlante? Not so much.With a little help from India’s Mahindra Group, who rescued Pininfarina in late 2015, and the Croatian EV electric geniuses at Rimac, comes the sensational Pininfarina Battista hypercar. Packing 1,900 hp and 1,696 ft lbs of torque from its 120 kwh lithium-ion battery pack and quad motors, this truly-gorgeous electric two-seat coupe can slingshot from zero to 60 mph in 1.8 seconds, and run zero to 186 mph in 12 seconds. Flat out, it’ll hit 217 mph before the electronic nannies kick in. And the range is over 230 miles.The first of the 150 cars being built—priced starting at $2.2 million each—has already been delivered. Want something really special? There’s a lavishly equipped Anniversario edition, of which only five will be produced. They’re priced closer to $2.9 million but, alas, all have been sold. H.W.',
+    },
+    {
+      id:21,
+      title:'Ferrari Daytona SP3',
+      img:'https://robbreport.com/wp-content/uploads/2021/12/2-24.jpg?w=1000',
+      info:'The Icona series of limited-production models tips a hat to the past by wrapping modern underpinnings with retro-futuristic skins. The third Icona to hail from Modena is the Daytona SP3, which recalls the Ferrari 330 P4s which finished first, second, and third at the 24 Hours of Daytona in 1967.Though its air intakes and aerodynamics are functional, the spirit of the SP3 is strictly nostalgic—specifically its naturally aspirated V-12 that revs to 9,500 rpm and produces 829 horsepower. From its swollen fenders to its dramatically straked rear end, the $2.2 million Daytona SP3 will serve as kinetic art when its 599 owners take delivery of their special steeds.',
+    }
+
+
+  ];
