2becb13b025ee60b981654f4ca2cf19aa0b4324d
Shreyascr5 <shreyas050801@gmail.com>
2023-09-12 07:40:02 +0530

    added feature
    added feature

--- src/Components/Cars.js
+++ src/Components/Cars.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import data from '../Data.js';
 
 function Cars() {
+<<<<<<< Updated upstream
   const cars = data.map((car) => (
     <div className='car-container' key={car.id}>
       <img src={car.img} alt="car" width={300} height={150} />
@@ -12,6 +13,29 @@ function Cars() {
       <Link to={`/car/${car.id}`}>
         <button>View more</button>
       </Link>
+=======
+  const showMore=(id)=>{
+    const more=data.map((e)=>{
+      const{id,title,img,info}=e;
+      return(
+        <div className="car" key={id}>
+          <img src={img} alt={title} />
+          <h3>{title}</h3>
+          <p>{info}</p>
+        </div>
+      )
+    })
+  }
+  const car=data.map((e)=>{
+    const{id,title,img}=e;
+    return(
+      <div className='car-container'>
+      <img src={img} alt="car" width={300} height={150} />
+      <h1>{title}</h1>
+      <p>Model-2023</p>
+      <br />
+      <button onClick={(id)=>showMore(id)}>Details</button>
+>>>>>>> Stashed changes
     </div>
   ));
 
