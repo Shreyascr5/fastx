2ea113aaeee47250afa84f82f747f89cfc04879b
Shreyascr5 <shreyas050801@gmail.com>
2023-09-12 08:53:13 +0530

    added data sheet

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import data from '../CarData';
 
 function Home() {
+  const allCars=data.map((c)=>(
+    <div className='car-container' key={c.id}>
+      <img src={c.img} alt="car" width={300} height={150} />
+      <h1>{c.title}</h1>
+      <p>Year- {c.year}</p>
+      <br />
+      {/* <Link to={`/car/${car.id}`}> */}
+        <button>View more</button>
+      {/* </Link> */}
+      </div>
+  ))
+    
+  
   return (
-    <div>Home</div>
+    <div className='home-grid'>
+      {allCars}
+    </div>
   )
-}
+  }
 
 export default Home
